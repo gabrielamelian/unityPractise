f539d41a8e4779ad43f5bf29e6d10ff8a8bb8712
gabrielamelian <gabrielamelian@gmail.com>
2017-01-17 16:11:28 +1300

    Adds hello world script

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	public string text = "Hello world";
+	public bool turnOn = true;
+	public float posX = 5.0f;
+	public int intTest = 5;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
