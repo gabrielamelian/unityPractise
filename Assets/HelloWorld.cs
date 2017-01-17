0823fc665055e44136f59f95d839388c4369d521
gabrielamelian <gabrielamelian@gmail.com>
2017-01-17 16:42:55 +1300

    Creates an array

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -7,14 +7,20 @@ public class HelloWorld : MonoBehaviour {
 	public bool turnOn = true;
 	public float posX = 5.0f;
 	public int intTest = 5;
-	
+	public string[] names;
+
 	// Use this for initialization
 	void Start () {
-
+		text = Test("Calling Test()"); //"Updated during start()";
+		print("hello world");
 	}
 
 	// Update is called once per frame
 	void Update () {
 
+	}
+
+	string Test (string value) {
+		return value + "Updated during test()";
 	}
 }
